fd3b8efccde5eb34eedb5cf2d39f87256ce24af9
Qiqi Zhao <qiqzhao@thoughtworks.com>
2020-04-28 18:52:12 +0800

    feat: custom scroll bar

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -6,6 +6,16 @@ class HomePage extends StatelessWidget {
     'https://t9.baidu.com/it/u=2268908537,2815455140&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1588668774&t=689f556bb44eb637fdf49bf75c5708db',
     'https://t9.baidu.com/it/u=1307125826,3433407105&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1588668774&t=710339aa6b51cb83112b7deb556c2122',
   ];
+
+  List<String> icons = [
+    'ac_unit',
+    'airport_shuttle',
+    'all_inclusive',
+    'beach_access',
+    'cake',
+    'free_breakfast'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -24,56 +34,56 @@ class HomePage extends StatelessWidget {
           ),
         ),
         Container(
-          padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
-          height: 150.0,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemExtent: 300,
-            itemCount: images.length,
-            itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                  title: Image.network(
-                images[index],
-                fit: BoxFit.contain,
-              ));
-            },
-          ),
-        ),
-        Container(
-          height: 130.0,
-          child: GridView.count(
-            crossAxisCount: 3,
-            childAspectRatio: 2.0,
-            children: <Widget>[
-              Column(
-                children: <Widget>[Icon(Icons.ac_unit), Text('unit')],
+          height: 650.0,
+          child: CustomScrollView(
+            slivers: <Widget>[
+              SliverToBoxAdapter(
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
+                  height: 150.0,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemExtent: 300,
+                    itemCount: images.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                          title: Image.network(
+                        images[index],
+                        fit: BoxFit.contain,
+                      ));
+                    },
+                  ),
+                ),
               ),
-              Column(
-                children: <Widget>[
-                  Icon(Icons.airport_shuttle),
-                  Text('airport')
-                ],
+              SliverPadding(
+                padding: const EdgeInsets.all(8.0),
+                sliver: new SliverGrid(
+                  gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    mainAxisSpacing: 10.0,
+                    crossAxisSpacing: 10.0,
+                    childAspectRatio: 4.0,
+                  ),
+                  delegate: new SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+                      return new Column(
+                        children: <Widget>[
+                          Text(icons[index])
+                        ],
+                      );
+                    },
+                    childCount: icons.length,
+                  ),
+                ),
               ),
-              Column(
-                children: <Widget>[
-                  Icon(Icons.all_inclusive),
-                  Text('inclusive')
-                ],
-              ),
-              Column(
-                children: <Widget>[
-                  Icon(Icons.beach_access),
-                  Text('beach access')
-                ],
-              ),
-              Column(
-                children: <Widget>[Icon(Icons.cake), Text('cake')],
-              ),
-              Column(
-                children: <Widget>[
-                  Icon(Icons.free_breakfast),
-                  Text('free breakfast')
-                ],
+              SliverFixedExtentList(
+                itemExtent: 50,
+                delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                    return new Container(child: Text('ddd'));
+                  },
+                  childCount: 30,
+                ),
               ),
             ],
           ),

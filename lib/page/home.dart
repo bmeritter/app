b3074969fec36ce871079bd273d3b46e92a158b5
Qiqi Zhao <qiqzhao@thoughtworks.com>
2020-04-28 17:49:56 +0800

    feat: scroll bar

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -8,80 +8,82 @@ class HomePage extends StatelessWidget {
   ];
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          height: 50,
-          padding: EdgeInsets.all(8),
-          child: TextField(
-            decoration: InputDecoration(
-              contentPadding: EdgeInsets.all(8),
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(30.0)),
-              prefixIcon: Icon(Icons.search),
-              suffixIcon: Icon(Icons.camera_alt),
-            ),
-          ),
-        ),
-        Container(
-          padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
-          height: 150.0,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemExtent: 300,
-            itemCount: images.length,
-            itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                  title: Image.network(
-                images[index],
-                fit: BoxFit.contain,
-              ));
-            },
-          ),
-        ),
-        Container(
-          height: 300.0,
-          child: GridView(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3, //横轴三个子widget
-                childAspectRatio: 2.0 //宽高比为1时，子widget
+    return Scrollbar(
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 50,
+              padding: EdgeInsets.all(8),
+              child: TextField(
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(8),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0)),
+                  prefixIcon: Icon(Icons.search),
+                  suffixIcon: Icon(Icons.camera_alt),
                 ),
-            children: <Widget>[
-              Column(
-                children: <Widget>[Icon(Icons.ac_unit), Text('unit')],
               ),
-              Column(
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
+              height: 150.0,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemExtent: 300,
+                itemCount: images.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                      title: Image.network(
+                    images[index],
+                    fit: BoxFit.contain,
+                  ));
+                },
+              ),
+            ),
+            Container(
+              height: 130.0,
+              child: GridView.count(
+                crossAxisCount: 3,
+                childAspectRatio: 2.0,
                 children: <Widget>[
-                  Icon(Icons.airport_shuttle),
-                  Text('airport')
+                  Column(
+                    children: <Widget>[Icon(Icons.ac_unit), Text('unit')],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.airport_shuttle),
+                      Text('airport')
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.all_inclusive),
+                      Text('inclusive')
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.beach_access),
+                      Text('beach access')
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[Icon(Icons.cake), Text('cake')],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.free_breakfast),
+                      Text('free breakfast')
+                    ],
+                  ),
                 ],
               ),
-              Column(
-                children: <Widget>[
-                  Icon(Icons.all_inclusive),
-                  Text('inclusive')
-                ],
-              ),
-              Column(
-                children: <Widget>[
-                  Icon(Icons.beach_access),
-                  Text('beach access')
-                ],
-              ),
-              Column(
-                children: <Widget>[Icon(Icons.cake), Text('cake')],
-              ),
-              Column(
-                children: <Widget>[
-                  Icon(Icons.free_breakfast),
-                  Text('free breakfast')
-                ],
-              ),
-            ],
-          ),
-        )
-      
-      ],
+            ),
+            Text('helleo'),
+          ],
+        ),
+      ),
     );
   }
 }

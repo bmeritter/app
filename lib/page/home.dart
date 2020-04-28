e0ea9d82f7c3575957cff6a00d0c7a93a6067227
Qiqi Zhao <qiqzhao@thoughtworks.com>
2020-04-28 17:25:38 +0800

    feat: horizontal scroll

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,22 +1,45 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  List<String> images = [
+    'https://t9.baidu.com/it/u=1307125826,3433407105&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1588668774&t=710339aa6b51cb83112b7deb556c2122',
+    'https://t9.baidu.com/it/u=2268908537,2815455140&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1588668774&t=689f556bb44eb637fdf49bf75c5708db',
+    'https://t9.baidu.com/it/u=1307125826,3433407105&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1588668774&t=710339aa6b51cb83112b7deb556c2122',
+  ];
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(8),
-      child: Column(
-        children: <Widget>[
-          TextField(
+    return Column(
+      children: <Widget>[
+        Container(
+          height: 50,
+          padding: EdgeInsets.all(8),
+          child: TextField(
             decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(8),
               border:
                   OutlineInputBorder(borderRadius: BorderRadius.circular(30.0)),
               prefixIcon: Icon(Icons.search),
               suffixIcon: Icon(Icons.camera_alt),
             ),
           ),
-        ],
-      ),
+        ),
+        Container(
+          padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
+          height: 200.0,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemExtent: 300,
+            itemCount: images.length,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                  title: Image.network(
+                images[index],
+                fit: BoxFit.contain,
+              ));
+            },
+          ),
+        )
+      ],
     );
   }
 }

a2e0d2e382499d584cd26aecf1b19a2c7e53c987
Qiqi Zhao <qiqzhao@thoughtworks.com>
2020-04-28 18:13:52 +0800

    fix scroll bar

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -8,82 +8,77 @@ class HomePage extends StatelessWidget {
   ];
   @override
   Widget build(BuildContext context) {
-    return Scrollbar(
-      child: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 50,
-              padding: EdgeInsets.all(8),
-              child: TextField(
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(8),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30.0)),
-                  prefixIcon: Icon(Icons.search),
-                  suffixIcon: Icon(Icons.camera_alt),
-                ),
-              ),
+    return Column(
+      children: <Widget>[
+        Container(
+          height: 50,
+          padding: EdgeInsets.all(8),
+          child: TextField(
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(8),
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(30.0)),
+              prefixIcon: Icon(Icons.search),
+              suffixIcon: Icon(Icons.camera_alt),
             ),
-            Container(
-              padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
-              height: 150.0,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemExtent: 300,
-                itemCount: images.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                      title: Image.network(
-                    images[index],
-                    fit: BoxFit.contain,
-                  ));
-                },
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
+          height: 150.0,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemExtent: 300,
+            itemCount: images.length,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                  title: Image.network(
+                images[index],
+                fit: BoxFit.contain,
+              ));
+            },
+          ),
+        ),
+        Container(
+          height: 130.0,
+          child: GridView.count(
+            crossAxisCount: 3,
+            childAspectRatio: 2.0,
+            children: <Widget>[
+              Column(
+                children: <Widget>[Icon(Icons.ac_unit), Text('unit')],
               ),
-            ),
-            Container(
-              height: 130.0,
-              child: GridView.count(
-                crossAxisCount: 3,
-                childAspectRatio: 2.0,
+              Column(
                 children: <Widget>[
-                  Column(
-                    children: <Widget>[Icon(Icons.ac_unit), Text('unit')],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.airport_shuttle),
-                      Text('airport')
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.all_inclusive),
-                      Text('inclusive')
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.beach_access),
-                      Text('beach access')
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[Icon(Icons.cake), Text('cake')],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.free_breakfast),
-                      Text('free breakfast')
-                    ],
-                  ),
+                  Icon(Icons.airport_shuttle),
+                  Text('airport')
                 ],
               ),
-            ),
-            Text('helleo'),
-          ],
+              Column(
+                children: <Widget>[
+                  Icon(Icons.all_inclusive),
+                  Text('inclusive')
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Icon(Icons.beach_access),
+                  Text('beach access')
+                ],
+              ),
+              Column(
+                children: <Widget>[Icon(Icons.cake), Text('cake')],
+              ),
+              Column(
+                children: <Widget>[
+                  Icon(Icons.free_breakfast),
+                  Text('free breakfast')
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
